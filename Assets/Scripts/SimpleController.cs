335be7027e81d21be9b5083979d4001d738d95b6
uwuJaver <ieva.stonciute@gmail.com>
2023-11-07 13:24:36 +0100

    fix merge

--- Assets/Scripts/SimpleController.cs
+++ Assets/Scripts/SimpleController.cs
@@ -18,13 +18,8 @@ public class SimpleController : MonoBehaviour
     private bool isGrounded = false;
     public Animator animator;
 
-    [Tooltip("Which things count as buttons?")]
-    public LayerMask buttonLayer;
-
     private Vector3 moveDirection = Vector3.zero;
 
-
-    public Animator animator;
 
     [Tooltip("Which things count as buttons?")]
     public LayerMask buttonLayer;
@@ -128,44 +123,7 @@ public class SimpleController : MonoBehaviour
 
     }
 
-    // Look for a button under our feet.
-    private void FindButton()
-    {
-        // Make a ray that points down
-        Ray downRay = new Ray(transform.position + Vector3.up, Vector3.down);
-        RaycastHit hit;
-
-        // Check if it hit a button
-        if (Physics.Raycast(downRay, out hit, 10f, buttonLayer))
-        {
-            // We only act the first time we touch a given button
-            if (!onButton)
-            {
-                OnButtonPress(hit.collider.gameObject);
-                onButton = true; // Ensures we only do one button press
-            }
-        }
-
-        else // If we are not standing on a button, reset onButton
-        {
-            if (onButton && lastSeenButton != null)
-            {
-                lastSeenButton.Release();
-                lastSeenButton = null;
-            }
-            onButton = false;
-        }
-    }
-
-    // When we are on a button, call its OnHit method
-    private void OnButtonPress(GameObject button)
-    {
-        Button buttonScript = button.GetComponent<Button>();
-        lastSeenButton = buttonScript;
-        buttonScript.OnHit();
-    }
-
-   
+  
     // Built-in ground check is bad, so use raycast instead
     private bool GroundControl()
     {
